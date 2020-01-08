0d33e5db330552102b18c419fe582e6b25385338
Dabra56 <david.alexandre.brassard@gmail.com>
2020-01-07 21:52:48 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Setting up the empty matrix, the getters and setter functions
+# Only difference from original codes apart from names is the default X value
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(,,,)) {
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+# We are setting the getters and setters of the caching function
+        get <- function() x
+        setinv <- function(inv) i <<- inv
+        getinv <- function() i
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
-
-
-## Write a short comment describing this function
-
+# Only change is in the mean calculation that is in this case the Solve function
+# Since we assume its always invertible, im not testing for it
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinv()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data)
+        x$setinv(i)
+        i
 }
+
+#Trying out the function for spin
+matrix_inv <- matrix(1:4,nrow=2,ncol=2)
+cache_matrix <- makeCacheMatrix(matrix_inv)
+
+cacheSolve(cache_matrix)
+#Trying out the condition
+cacheSolve(cache_matrix)
